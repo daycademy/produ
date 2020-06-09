e6909c70d27a650e7eba4e33b49bdc84f1e9fcf5
florianwoelki <florianwoelki@gmx.de>
2020-06-09 14:39:54 +0200

    build: add vue-cirrus library

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+import VueCirrus from 'vue-cirrus';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import 'vue-cirrus/dist/vue-cirrus.css';
+
+Vue.use(VueCirrus);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <v-btn color="success">Hello World</v-btn>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
